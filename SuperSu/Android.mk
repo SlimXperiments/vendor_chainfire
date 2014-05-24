b50a019c429d423d321ea8f167933d05b4ecf795
Anthony King <anthonydking@gmail.com>
2014-05-24 10:15:49 +0100

    Initial push of SuperSu
    V1.99

---
+++ SuperSu/Android.mk
@@ -0,0 +1,56 @@
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+LOCAL_PATH := $(call my-dir)
+ifneq ($(filter arm arm64, $(TARGET_ARCH)),)
+    local_arch := arm
+else ifneq ($(filter x86 x86_64, $(TARGET_ARCH)),)
+    local_arch := x86
+endif
+
+ifneq ($(local_arch),)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := SuperSu
+LOCAL_SRC_FILES := common/$(LOCAL_MODULE).apk
+LOCAL_MODULE_CLASS := APPS
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
+LOCAL_CERTIFICATE := platform
+LOCAL_REQUIRED_MODULES := su daemonsu 99SuperSuDaemon 
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := 99SuperSuDaemon
+LOCAL_SRC_FILES := common/99SuperSUDaemon
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_PATH := $(TARGET_OUT_ETC)/init.d
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := su
+LOCAL_SRC_FILES := $(local_arch)/su
+LOCAL_MODULE_CLASS := EXECUTABLES
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_PATH := $(TARGET_OUT_OPTIONAL_EXECUTABLES)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := daemonsu
+LOCAL_SRC_FILES := $(local_arch)/su
+LOCAL_MODULE_CLASS := EXECUTABLES
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_PATH := $(TARGET_OUT_OPTIONAL_EXECUTABLES)
+include $(BUILD_PREBUILT)
+
+endif
